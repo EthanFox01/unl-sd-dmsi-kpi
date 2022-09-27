b469c9a37c6c3cb10c2e9809f649a51748719c83
Zckerkman <zkerkman@dmsi.com>
2022-09-27 14:12:34 -0500

    local login page

---
+++ src/components/LocalDevApp/styles/StyledLayout.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Layout from '@wedgekit/layout';
+
+const StyledLayout = styled(Layout.Grid)`
+  column-gap: 20px;
+  row-gap: 50px;
+`;
+
+export default StyledLayout;
