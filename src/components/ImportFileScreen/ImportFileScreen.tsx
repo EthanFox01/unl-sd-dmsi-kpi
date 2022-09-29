413bdcb2c4918729936d2c20959a5d991a05532f
Divith Rajagopal <divithr@gmail.com>
2022-09-29 14:23:57 -0500

    Base UI for import file screen

--- src/components/ImportFileScreen/ImportFileScreen.tsx
+++ src/components/ImportFileScreen/ImportFileScreen.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import Button from '@wedgekit/button';
 
 const ImportFileScreen = () => {
+  const [domain, setDomain] = React.useState('primary');
+
   return (
-    <div>
-      <button type="button">Browse Files</button>
+  <><h1 style={{
+    fontSize: '20px',
+    position: 'absolute', left:'845px', top: '320px'}}
+    >Drag and drop files here</h1>
+    <div style={{
+    fontSize: '20px',
+    position: 'absolute', left:'930px', top: '390px'}}>or</div>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        color: 'blue'
+      }}>
+      <Button
+        domain={domain}
+        onClick={() => setDomain(domain === 'primary' ? 'secondary' : 'primary')}
+      >
+        Browse Files
+      </Button>
     </div>
+    <div style={{
+    fontSize: '15px',
+    position: 'absolute', left:'890px', bottom: '400px'}}>(csv,tsv,xls,xlsx)</div>
+    </>
   );
 };
+
 
 export default ImportFileScreen;
