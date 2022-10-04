fd0bee70dd138e247ac6e8ef0c2600957fd84795
Brian Bui <bui.brian99@gmail.com>
2022-10-04 15:43:48 -0500

    Add wedgekit layout

--- src/screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import Layout from '@wedgekit/layout';
+import Button from "@wedgekit/button";
+
 import SidebarDiv from './styles/SidebarDiv'
 import HeaderWrapper from './styles/HeaderWrapper'
 import ChartSelectWrapper from './styles/ChartSelectWrapper';
@@ -16,26 +19,77 @@ const DashboardScreen = () => {
     }
 
     return (
-        <div>
-            <SidebarDiv>
-                <button onClick={handleImportDataClick}>Import Data</button>
-            </SidebarDiv>
-            <MainPageWrapper>
+        <Layout.App 
+            // header={<HeaderContainer>Header</HeaderContainer>}
+            resource={<SidebarDiv>
+                        <Button onClick={handleImportDataClick}>Import Data</Button>
+                    </SidebarDiv>}
+            resourceMounted
+            resourceType="drawer"
+            drawerLeft
+            drawerWidth="10vw"
+        >
+            {() => <MainPageWrapper>
                 <h1>Charts</h1>
-                <ChartSelectWrapper onClick={handleChartClick}>
-                    <HeaderWrapper>
-                        <h3>Pie Chart</h3>
-                        <h1>Title</h1>
-                        <h4>Secondary text</h4>
-                    </HeaderWrapper>
-                    <br></br>
-                    <img 
-                        src="https://www.tibco.com/sites/tibco/files/media_entity/2022-01/PieChart-01.svg"
-                        alt="Pie Chart"
-                    />
-                </ChartSelectWrapper>
-            </MainPageWrapper>
-        </div>
+                <Layout.Grid
+                    areas={['chart1 chart2 chart3']}
+                    columns={['']}
+                    rows={['50vh']}
+                    // areasLg={['chart1 chart2', 'chart3 .']}
+                    rowsLg={['40vh', '40vh', '40vh']}
+                    areasMd={['chart1', 'chart2', 'chart3']}
+                    rowsMd={['65vh', '65vh', '65vh']}
+                    areasSm={['chart1', 'chart2', 'chart3']}
+                    rowsSm={['50vh', '50vh', '50vh']}
+                >
+                    <Layout.Section area="chart1">
+                        <ChartSelectWrapper onClick={handleChartClick}>
+                            <HeaderWrapper>
+                                <h3>Pie Chart</h3>
+                                <h1>Title</h1>
+                                <h4>Secondary text</h4>
+                            </HeaderWrapper>
+                            <br></br>
+                            <img 
+                                src="https://www.tibco.com/sites/tibco/files/media_entity/2022-01/PieChart-01.svg"
+                                alt="Pie Chart"
+                                style={{ width: '100%' }}
+                            />
+                        </ChartSelectWrapper>
+                    </Layout.Section>
+                    <Layout.Section area="chart2">
+                        <ChartSelectWrapper onClick={handleChartClick}>
+                            <HeaderWrapper>
+                                <h3>Pie Chart</h3>
+                                <h1>Title</h1>
+                                <h4>Secondary text</h4>
+                            </HeaderWrapper>
+                            <br></br>
+                            <img 
+                                src="https://www.tibco.com/sites/tibco/files/media_entity/2022-01/PieChart-01.svg"
+                                alt="Pie Chart"
+                                style={{ width: '100%' }}
+                            />
+                        </ChartSelectWrapper>
+                    </Layout.Section>
+                    <Layout.Section area="chart3">
+                        <ChartSelectWrapper onClick={handleChartClick}>
+                            <HeaderWrapper>
+                                <h3>Pie Chart</h3>
+                                <h1>Title</h1>
+                                <h4>Secondary text</h4>
+                            </HeaderWrapper>
+                            <br></br>
+                            <img 
+                                src="https://www.tibco.com/sites/tibco/files/media_entity/2022-01/PieChart-01.svg"
+                                alt="Pie Chart"
+                                style={{ width: '100%' }}
+                            />
+                        </ChartSelectWrapper>
+                    </Layout.Section>
+                </Layout.Grid>
+            </MainPageWrapper>}
+        </Layout.App>
     );
 }
 
--- src/screens/DashboardScreen/styles/ChartSelectWrapper.tsx
+++ src/screens/DashboardScreen/styles/ChartSelectWrapper.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const ChartSelectWrapper = styled.div`
-    height: 20%;
-    width: 45%;
+    height: 100%;
+    width: 100%;
     border-radius: 10px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
     overflow: hidden;
