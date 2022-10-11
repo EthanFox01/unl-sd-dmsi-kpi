220723a4d0d54752bc9bbf603e48a3bc9622d258
Ethan Fox <fox.ethan2001@gmail.com>
2022-10-11 15:22:01 -0500

    Set up page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { History } from 'history';
-import ImportFileScreen from "./components/ImportFileScreen"
+import ImportFileScreen from "./components/ImportFileScreen";
 import { DashboardScreen } from './screens/DashboardScreen';
+import ChartViewerScreen from './components/screen/ChartViewerScreen';
 
 import { ForceSessionData } from './@types/force';
 
@@ -13,10 +18,16 @@ function App({
   history: History<unknown>;
   sessionData?: ForceSessionData;
 }) {
+
   return (
     <Router history={history}>
-      <DashboardScreen/>
+      {/* <DashboardScreen /> */}
       {/*<div><ImportFileScreen/></div> */}
+      <Switch>
+        <Route path="/import" component={ImportFileScreen} />
+        <Route path="/viewer" component={ChartViewerScreen}/>
+        <Route path="/" component={DashboardScreen} />
+      </Switch>
     </Router>
   );
 }
--- src/screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'react-router-dom';
 
 import Layout from '@wedgekit/layout';
 import Button from "@wedgekit/button";
