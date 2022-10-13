b2a94adfe94b33401707f7fa72978794e5de9a22
Andrea McGrane <andrea.mcgrane@gmail.com>
2022-10-13 17:12:14 -0500

    Parsed file & return a map of first 2 num columns

--- src/feature/processNumericalFiles.ts
+++ src/feature/processNumericalFiles.ts
@@ -1,10 +1,5 @@
-import { Dictionary } from "@reduxjs/toolkit";
 
-function firstTwoNumericalColumns (jsonFile) : Array<String> {
-    var isNumericalColumn = false;
-    var numericalColumnNumber = 0;
-    var partialArray;
-
+function parseFile(jsonFile) : Map<String, any> {
     var keyArray: Array<String> = [];
     var valueArray: Array<any> = [];
 
@@ -15,23 +10,46 @@ function firstTwoNumericalColumns (jsonFile) : Array<String> {
         Object.entries(jsonFile[i]).forEach(([key, value]) => valueArray.push(value));
     }
 
-    for(let column = 0; column < jsonFile.length; column++){
-        if(onlyNumbers(jsonFile[column])){
-            isNumericalColumn = true;
-            partialArray.push(jsonFile[column]);
-        }
-        if(isNumericalColumn){
-            numericalColumnNumber++;
-        }
-        if(numericalColumnNumber == 2){
-            break;
-        }
-    }
-    return partialArray;
+    var valueArrayByRow = [];
+       while(valueArray.length){
+        valueArrayByRow.push(valueArray.splice(0, (keyArray.length)));
+       }
+    return firstTwoNumericalColumns(keyArray, valueArray);
 }
 
-function onlyNumbers(array) {
-    return array.every(element => {
-      return typeof element === 'number';
-    });
-  }
+function firstTwoNumericalColumns (keyArray, valueArrayByRow) : Map<String, number> {
+    var numericalColumn1 = -1;
+    var numericalColumn2 = -1;
+    var numericalColumnMap: Map<String, any> = new Map<String, any>();
+
+    for(let row = 0; row < valueArrayByRow.length; row++){
+        for(let column = 0; column < valueArrayByRow[0].length; column++){
+            if(typeof valueArrayByRow[row][column] === "number"){
+                if(numericalColumn1 == -1){
+                    numericalColumn1 = column;
+                }
+                else{
+                    numericalColumn2 = column;
+                }
+            }
+            if(numericalColumn1 != -1 && numericalColumn2 != -1){
+              break;
+            }
+        }
+        if(numericalColumn1 != -1 && numericalColumn2 != -1){
+          break;
+        }
+    }
+    numericalColumnMap.set(keyArray[numericalColumn1], numericalColumnValues(valueArrayByRow, numericalColumn1));
+    numericalColumnMap.set(keyArray[numericalColumn2], numericalColumnValues(valueArrayByRow, numericalColumn2));
+
+    return numericalColumnMap;
+}
+
+function numericalColumnValues(valueArrayByRow, numericalColumnNumber) : Array<any>{
+    var columnValues: Array<any>;
+    for(let i = 0; i < valueArrayByRow.length; i++){
+        columnValues.push(valueArrayByRow[i][numericalColumnNumber]);
+       }
+    return columnValues;
+}
